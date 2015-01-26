6c0ba92d4bbcfb784e051a830a57221ecb6d43e4
amonteiro <anthony.monteiro@lyra-network.com>
2015-01-26 17:22:52 +0100

    Ajout du service par binding

---
+++ servicebinding/src/main/java/temp/com/servicebinding/MainActivity.java
@@ -0,0 +1,160 @@
+package temp.com.servicebinding;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.formation.utils.ToastUtils;
+
+import temp.com.servicebinding.services.UpdateDataService;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    //composant graphique
+    private TextView tv;
+
+    // ServiceConnection permet de gérer l'état du lien entre l'activité et le service.
+    private ServiceConnection serviceConnection;
+    //L'instance du service
+    private UpdateDataService updateDataService;
+
+    //-----------------------
+    //View
+    //-------------------
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tv.setText("Activité prête");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Sinon impossible de tuer l'activité
+        if (serviceConnection != null) {
+            unbindService(serviceConnection);
+        }
+        updateDataService = null;
+        serviceConnection = null;
+    }
+
+    public void updateTextView(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tv.setText(text);
+            }
+        });
+    }
+
+    /**
+     * met à jour l'état du service sur le TextView
+     */
+    public void refreshServiceValue() {
+        if (updateDataService != null) {
+            updateTextView(updateDataService.getServiceTimeExecutionInSecond() + "");
+        }
+        else {
+            updateTextView("Aucun service bindé");
+        }
+    }
+
+    /* ---------------------------------
+    // Service
+    // -------------------------------- */
+
+    /**
+     * Lance ou récupère l'instance du service
+     */
+    private void bindToService() {
+
+        if (serviceConnection == null) {
+
+            serviceConnection = new ServiceConnection() {
+
+                //le service s'est déconnecté
+                public void onServiceDisconnected(ComponentName name) {
+                    updateDataService = null;
+                    if (MainActivity.this != null) {
+                        ToastUtils.showToast(MainActivity.this, "Déconnecté du service", Toast.LENGTH_LONG);
+                        updateTextView("Le service s'est arreté");
+                    }
+                }
+
+                //le service se connecte
+                public void onServiceConnected(ComponentName arg0, IBinder binder) {
+                    //on récupère l'instance du service dans l'activité
+                    updateDataService = ((UpdateDataService.UpdateDataServiceBinder) binder).getUpdateDataService();
+
+                    ToastUtils.showToast(MainActivity.this, "Service bindé", Toast.LENGTH_LONG);
+
+                    refreshServiceValue();
+
+                }
+            };
+        }
+        //démarre le service si il n'est pas démarré
+        //Le binding du service est configuré avec "BIND_AUTO_CREATE" ce qui normalement
+        //démarre le service si il n'est pas démarrer, la différence ici est que le fait de
+        //démarrer le service par "startService" fait que si l'activité est détruite, le service
+        //reste en vie
+        startService(new Intent(this, UpdateDataService.class));
+        Intent intent = new Intent(this, UpdateDataService.class);
+        //lance le binding du service
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        updateTextView("Démarrage du service");
+    }
+
+    private void stopService() {
+        updateTextView("Arret du service");
+        //on detruit le service
+        if (updateDataService != null) {
+            updateDataService.stopSelf();
+            updateDataService = null;
+        }
+        if (serviceConnection != null) {
+            unbindService(serviceConnection);
+            serviceConnection = null;
+        }
+
+    }
+
+    /* ---------------------------------
+    // Evenements
+    // -------------------------------- */
+
+    @Override
+    public void onClick(final View v) {
+        if (v.getId() == R.id.startService) {
+            bindToService();
+        }
+        else if (v.getId() == R.id.stopService) {
+            stopService();
+        }
+        else if (v.getId() == R.id.updateTime) {
+            refreshServiceValue();
+        }
+        else if (v.getId() == R.id.newActivity) {
+            //On crée une nouvelle Activité ou on récuperera le même service déjà lancé
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+}
---
+++ servicebinding/src/main/java/temp/com/servicebinding/services/UpdateDataService.java
@@ -0,0 +1,76 @@
+package temp.com.servicebinding.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class UpdateDataService extends Service {
+
+    private IBinder iBinder = null; //l'instance du binder correspondant à notre service
+
+    //Note l'heure de création du service
+    private long creationTime;
+
+    //-----------------------
+    // Service
+    //-------------------
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //au démarrage du service, on créé le binder en envoyant le service
+        iBinder = new UpdateDataServiceBinder(this);
+        creationTime = System.currentTimeMillis();
+
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, final int flags, final int startId) {
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(final Intent intent) {
+        return iBinder;
+    }
+
+    /* ---------------------------------
+    // Method
+    // -------------------------------- */
+
+    /**
+     *
+     * @return Le temps en second d'existence du service
+     */
+    public long getServiceTimeExecutionInSecond() {
+
+        long difference = System.currentTimeMillis() - creationTime;
+        return difference / 1000;
+    }
+
+    /* ---------------------------------
+    // Binder
+    // -------------------------------- */
+
+    public class UpdateDataServiceBinder extends Binder {
+
+        private UpdateDataService updateDataService;
+
+        //on recoit l'instance du service
+        public UpdateDataServiceBinder(UpdateDataService updateDataService) {
+            super();
+            this.updateDataService = updateDataService;
+        }
+
+        /* ---------------------------------
+        // Getter / Setter
+        // -------------------------------- */
+
+        /** @return l'instance du service */
+        public UpdateDataService getUpdateDataService() {
+            return updateDataService;
+        }
+    }
+}
