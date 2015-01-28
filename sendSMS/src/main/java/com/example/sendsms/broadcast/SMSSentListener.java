435fdb1be5579e62a61cabb14078f1dd59b69412
amonteiro <anthony.monteiro@lyra-network.com>
2015-01-28 14:39:43 +0100

    Correction SMSSent

--- sendSMS/src/main/java/com/example/sendsms/broadcast/SMSSentListener.java
+++ sendSMS/src/main/java/com/example/sendsms/broadcast/SMSSentListener.java
@@ -7,15 +7,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.telephony.SmsManager;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by amonteiro on 05/12/2014.
  */
 public class SMSSentListener extends BroadcastReceiver {
 
-    public static final String SENT_SMS_ACTION_NAME = "Message_sent";
+    public static final String SENT_SMS_ACTION_NAME = "SMS_SENT";
     public static final String DELIVERED_SMS_ACTION_NAME = "SMS_DELIVERED";
 
     @Override
@@ -65,18 +68,39 @@ public class SMSSentListener extends BroadcastReceiver {
 
     public static void sendSMS(final Context context, String phoneNumber, String message) {
 
+        if (!canSendSMS(context)) {
+            Toast.makeText(context, "Le device ne permet pas l'envoie de SMS", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         PendingIntent sentPI = PendingIntent.getBroadcast(context, 0, new Intent(SENT_SMS_ACTION_NAME), 0);
         PendingIntent deliveredPI = PendingIntent.getBroadcast(context, 0, new Intent(DELIVERED_SMS_ACTION_NAME), 0);
 
-        SMSSentListener smsSentListener = new SMSSentListener();
+        final SMSSentListener smsUtils = new SMSSentListener();
         //on s'enregistre à l'envoie du sms
-        context.registerReceiver(smsSentListener, new IntentFilter(SMSSentListener.SENT_SMS_ACTION_NAME));
+        context.registerReceiver(smsUtils, new IntentFilter(SENT_SMS_ACTION_NAME));
         // Receiver for Delivered SMS.
-        context.registerReceiver(smsSentListener, new IntentFilter(DELIVERED_SMS_ACTION_NAME));
-        //Penser à se desenregistrer
-        //context.unregisterReceiver(smsSentListener);
+        context.registerReceiver(smsUtils, new IntentFilter(DELIVERED_SMS_ACTION_NAME));
 
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+        //On divise notre messege en plusieurs SMS en fonction du format
+        ArrayList<String> parts = sms.divideMessage(message);
+
+        ArrayList<PendingIntent> sendList = new ArrayList<>();
+        sendList.add(sentPI);
+
+        ArrayList<PendingIntent> deliverList = new ArrayList<>();
+        deliverList.add(deliveredPI);
+
+        sms.sendMultipartTextMessage(phoneNumber, null, parts, sendList, deliverList);
+
+        //Penser à se desenregistrer
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                context.unregisterReceiver(smsUtils);
+            }
+        }, 10000);//on se desinscrit au bout de 10s
+
     }
 }
